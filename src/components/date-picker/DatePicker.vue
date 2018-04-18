fd6afa7d37b79497786a27ed2896b26527325d36
toBeTheLight <yl1991web@126.com>
2018-04-18 11:34:05 +0800

    refactor: TryDatePicker

--- src/components/date-picker/DatePicker.vue
+++ src/components/date-picker/DatePicker.vue
@@ -2,43 +2,76 @@
 </template>
 
 <script>
-import { boundaryTime, fillDatePanel } from './utils'
+import { dateDetails, boundaryTime, fillDateMatrix } from './utils'
 
 export default {
   data () {
     return {
-      year: '',
-      month: '',
-      dateRows: [[], [], [], [], [], []]
+      dateRightNow: null,
+      dateSelected: null,
+      dateShown: null,
+      showYear: '',
+      showMonth: '',
+      nowYear: '',
+      nowMonth: '',
+      nowDate: '',
+      dateMatrix: []
     }
   },
   methods: {
-    initDate (date) {
-      let res = boundaryTime(date)
-      let {now} = boundaryTime(date)
-      this.year = now.year
-      this.month = now.month
-      this.initPanel(res)
+    initMain (date) {
+      this.initDateRightNow()
+      this.initDateSelected()
+      this.initDateShown()
+      this.initPanel()
     },
-    initPanel (res) {
-      this.dateRows = fillDatePanel(res)
+    // 当前时间
+    initDateRightNow () {
+      this.dateRightNow = dateDetails(new Date())
+      console.log(this.dateRightNow)
+    },
+    // 选择时间 v-model
+    initDateSelected () {
+      // 同步当前时间
+      if (!this.dateSelected) {
+        this.dateSelected = Object.assign({}, this.dateRightNow)
+      } else {
+        // 或传入时间
+
+      }
+    },
+    initDateShown () {
+      this.dateShown = Object.assign({}, this.dateSelected)
+    },
+    initPanel () {
+      let res = boundaryTime(new Date(`${this.dateShown.year}/${this.dateShown.month}/01`))
+      this.dateMatrix = fillDateMatrix(res)
+    },
+    changeDateShown (num, type) {
+      switch (type) {
+        case 'year':
+          this.changeYear(num)
+          break
+        case 'month':
+          this.changeMonth(num)
+          break
+      }
     },
     changeYear (num) {
-      this.date = new Date(`${this.year + num}/${this.month}/01`)
-      this.initDate(this.date)
+      this.dateShown.year += num
+      this.initPanel(new Date(`${this.dateShown.year}/${this.dateShown.month}/01`))
     },
     changeMonth (num) {
-      let month = this.month + num
+      let month = this.dateShown.month + num
       if (month <= 0) {
-        this.year -= 1
+        this.dateShown.year -= 1
         month = 12
       } else if (month >= 12) {
         month = month % 12
-        this.year += 1
+        this.dateShown.year += 1
       }
-      this.month = month
-      this.date = new Date(`${this.year}/${this.month}/01`)
-      this.initDate(this.date)
+      this.dateShown.month = month
+      this.initPanel(new Date(`${this.dateShown.year}/${this.dateShown.month}/01`))
     },
     selectDate (cell) {
       alert(`${cell.year}/${cell.month}/${cell.date}`)
@@ -46,7 +79,7 @@ export default {
   },
   created () {
     this.date = new Date()
-    this.initDate(this.date)
+    this.initMain(this.date)
   }
 }
 </script>
--- src/components/date-picker/utils.js
+++ src/components/date-picker/utils.js
@@ -2,7 +2,7 @@
  * 将date:Date拆为year,month,date,day
  * @param {*} dateDetails
  */
-function dateDetails (date) {
+export function dateDetails (date) {
   if (typeof date === 'number') {
     date = new Date(date)
   }
@@ -15,22 +15,22 @@ function dateDetails (date) {
 }
 /**
  * 以现在的时间为准，获取上月最后一天，当月第一天、当天、最后一天、下月第一天的拆分时间
- * @param {*} dateNow
+ * @param {*} dateReference
  */
-export function boundaryTime (dateNow) {
+export function boundaryTime (dateReference) {
   let nowMonthStartDate
   let nowMonthEndDate
   let prevMonthEndDate
   let nextMonthStartDate
-  let now = dateDetails(dateNow)
+  let reference = dateDetails(dateReference)
   const oneDay = 24 * 60 * 60 * 1000
   // 当月起始
-  nowMonthStartDate = new Date(`${now.year}/${now.month}/${1}`)
+  nowMonthStartDate = new Date(`${reference.year}/${reference.month}/${1}`)
   // 下月起始
-  if (now.month === 12) {
-    nextMonthStartDate = new Date(`${now.year + 1}/${1}/${1}`)
+  if (reference.month === 12) {
+    nextMonthStartDate = new Date(`${reference.year + 1}/${1}/${1}`)
   } else {
-    nextMonthStartDate = new Date(`${now.year}/${now.month + 1}/${1}`)
+    nextMonthStartDate = new Date(`${reference.year}/${reference.month + 1}/${1}`)
   }
   // 当月结束
   nowMonthEndDate = new Date(nextMonthStartDate.getTime() - oneDay)
@@ -43,13 +43,17 @@ export function boundaryTime (dateNow) {
   return {
     prevMonthEnd,
     nowMonthStart,
-    now,
+    reference,
     nowMonthEnd,
     nextMonthStart
   }
 }
 
-export function fillDatePanel ({prevMonthEnd, nowMonthStart, now, nowMonthEnd, nextMonthStart}) {
+/**
+ * 填充当月时间6*7矩阵，单个元素为拆分时间
+ * @param {boundaryTime} boundaryTime
+ */
+export function fillDateMatrix ({prevMonthEnd, nowMonthStart, reference, nowMonthEnd, nextMonthStart}) {
   let dateRows = []
   // 按行填充
   let fill = (startRowNum, day, type) => {
@@ -73,7 +77,7 @@ export function fillDatePanel ({prevMonthEnd, nowMonthStart, now, nowMonthEnd, n
   }
   // 从当月起始日期填充值最后一天
   while (day.date <= nowMonthEnd.date) {
-    fill(0, day, 'now')
+    fill(0, day, 'reference')
   }
   // 填充前一月
   let prevRowNum = 0
